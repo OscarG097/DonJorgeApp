dbc6555aed68177db716b19732998bfc3c07b408
OscarG097 <oscar010797@gmail.com>
2025-12-04 13:46:05 -0300

    feat: first commit - structure app

---
+++ app/src/main/java/com/bebidas/donjorge/NewProduct.kt
@@ -0,0 +1,11 @@
+package com.bebidas.donjorge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewProduct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_product)
+    }
+}
