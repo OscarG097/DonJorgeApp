d23e0838c35cb0a57bba9d45cab8fdd6f8be17b1
OscarG097 <oscar010797@gmail.com>
2025-12-06 02:01:09 -0300

    feat: update save product with image

--- app/src/main/java/data/AppDatabase.kt
+++ app/src/main/java/data/AppDatabase.kt
@@ -1,11 +1,32 @@
 package com.bebidas.donjorge.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+// Importar la Entidad Producto (necesaria para @Database)
+import com.bebidas.donjorge.data.Producto
+import com.bebidas.donjorge.data.ProductoDao
 
 @Database(entities = [Producto::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun productoDao(): ProductoDao
 
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "control_bebidas_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
